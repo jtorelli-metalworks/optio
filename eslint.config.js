1942792e6afef96beab68465bf943b71c2582f5e
jtorelli-metalworks <jtorelli-metalworks@users.noreply.github.com>
2026-05-28 12:42:37 -0700

    chore: ignore generated site output in lint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ export default tseslint.config(
     ignores: [
       "**/node_modules/**",
       "**/dist/**",
+      "**/out/**",
       "**/.next/**",
       "**/.turbo/**",
       "**/helm/**",
